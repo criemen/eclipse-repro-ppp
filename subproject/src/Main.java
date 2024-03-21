4267a612b210b4104f388dd7e2d4fb089ee02c0d
Cornelius Riemenschneider <cornelius@github.com>
2024-03-21 12:33:44 +0100

    Initial commit

---
+++ subproject/src/Main.java
@@ -0,0 +1,10 @@
+package src;
+
+import org.hamcrest.core.Is;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello, World...!");
+        Is is = new Is(null);
+    }
+}
---
+++ subproject/test/MainTest.java
@@ -0,0 +1,11 @@
+package test;
+
+import static org.junit.Assert.assertEquals;
+import org.junit.Test;
+
+public class MainTest {
+    @Test
+    public void test() {
+        assertEquals(1, 1);
+    }
+}
